75502bbab1690f38504ac91e57328dec353a95e7
kimxd <kim.sergmix@gmail.com>
2024-02-26 19:48:30 +0100

    4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,8 +4,12 @@ from brain_games.cli import welcome_user
 
 def welcome():
     print('Welcome to the Brain Games!')
+
+
 def main():
     welcome()
     welcome_user()
+
+
 if __name__ == '__main__':
     main()
