e807f7c8f1ed9b2506915749cdf563caf826cbb2
kimxd <kim.sergmix@gmail.com>
2024-03-01 18:40:17 +0100

    fix

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,20 +3,20 @@
 import prompt
 import random
 number_of_steps = 3
-operators = ["+","-","*"]
 
 
-def brain_calc():
+def brain_even():
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
     print(f"Hello, {user_name}!")
-    print('What is the result of the expression?')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     for step in range(number_of_steps):
-        num1 = random.randint(1, 20)
-        num2 = random.randint(1, 20)
-        operator = random.choice(operators)
-        game_answer = eval(str(num1)+operator+str(num2))
-        user_answer = prompt.string(f'Question: {num1} {operator} {num2} \nYour answer: ')
+        num = random.randint(1, 20)
+        if num % 2 == 0:
+            game_answer = 'yes'
+        else:
+            game_answer = 'no'
+        user_answer = prompt.string(f'Question: {num} \nYour answer: ')
         if user_answer != game_answer:
             print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(
               user_answer, game_answer))
@@ -27,9 +27,5 @@ def brain_calc():
     print('Congratulations, {}!'.format(user_name))
 
 
-def main():
-    brain_calc()
-
-
 if __name__ == '__main__':
-    main()
+    brain_even()
