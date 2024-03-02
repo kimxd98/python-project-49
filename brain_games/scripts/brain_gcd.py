842514e9048d8f349a3999de9b893307099f0b2e
Sergey Kim <kimxd98>
2024-03-02 20:06:50 +0100

    'gcd'

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env
+
+import prompt
+import random
+import math
+number_of_steps = 3
+
+def show_wrong_answer(user_answer, game_answer, user_name):
+    print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(
+        user_answer, game_answer))
+    print("Let's try again, {}!".format(user_name))
+
+def brain_even():
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f"Hello, {user_name}!")
+    print('Find the greatest common divisor of given numbers.')
+    for step in range(number_of_steps):
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        game_answer = math.gcd(num1, num2)
+        user_answer = prompt.string(f'Question: {num1} {num2} \nYour answer: ')
+        if user_answer.isnumeric():
+            if int(user_answer) != int(game_answer):
+                show_wrong_answer(user_answer, game_answer, user_name)
+                return
+            else:
+                print('Correct!')
+        else:
+            show_wrong_answer(user_answer, game_answer, user_name)
+            return
+        print('Congratulations, {}!'.format(user_name))
+
+def main():
+    brain_even()
+
+if __name__ == '__main__':
+    main()
